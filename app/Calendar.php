90e30ce2c62f3d139e9b3e9238ca0727ab1fa266
taka027 <kanekotakashi2009@gmail.com>
2018-10-07 05:38:23 +0000

    カレンダーの月めくり

--- app/Calendar.php
+++ app/Calendar.php
@@ -18,10 +18,22 @@ class Calendar
         }
         $firstWeekDay = date("w", mktime(0, 0, 0, $month, 1, $year)); // 1日の曜日(0:日曜日、6:土曜日)
         $lastDay      = date("t", mktime(0, 0, 0, $month, 1, $year)); // 指定した月の最終日
+        // 前月
+	$prev = strtotime('-1 month',mktime(0, 0, 0, $month, 1, $year));
+        $prev_year = date("Y",$prev);
+        $prev_month = date("m",$prev);
+        // 翌月
+	$next = strtotime('+1 month',mktime(0, 0, 0, $month, 1, $year));
+        $next_year = date("Y",$next);
+        $next_month = date("m",$next);
         // 日曜日からカレンダーを表示するため前月の余った日付をループの初期値にする
         $day = 1 - $firstWeekDay;
         $this->html = <<< EOS
-<h1>{$year}年{$month}月</h1>
+<h2>
+<a class="btn btn-primary" href="/?year={$prev_year}&month={$prev_month}" role="button">&lt;前月</a>
+{$year}年{$month}月
+<a class="btn btn-primary" href="/?year={$next_year}&month={$next_month}" role="button">翌月&gt;</a>
+</h2>
 <table class="table table-bordered" style="table-layout:fixed;">
 <tr>
   <th scope="col">日</th>
